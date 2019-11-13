152b43e30bb58c1bd8bb1b12ac9be24abf4bf912
Oliver Gomes <ogomesphoto@gmail.com>
2019-11-12 23:36:21 -0500

    LandingPage add

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import LandingPage from "./components/LandingPage/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      <p>Test</p>
+      <LandingPage />
     </div>
   );
 }

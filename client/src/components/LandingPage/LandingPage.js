076c23533d5713c4cfb61a15a96ca001945542b3
Oliver Gomes <ogomesphoto@gmail.com>
2019-11-12 23:36:31 -0500

    create LandingPage

---
+++ client/src/components/LandingPage/LandingPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import TopNav from "../TopNav/TopNav";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <TopNav />
+      LandingPage
+    </div>
+  );
+}

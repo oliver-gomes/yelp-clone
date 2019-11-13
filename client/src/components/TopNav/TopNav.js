ef1cf79a94cac4699b69c91d1ae24a290997f523
Oliver Gomes <ogomesphoto@gmail.com>
2019-11-12 23:36:40 -0500

    create TopNav

---
+++ client/src/components/TopNav/TopNav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function TopNav() {
+  return (
+    <div>
+      <div>
+        <span>Write a Review</span>
+        <span>Events</span>
+      </div>
+      <div>
+        <span>Login</span>
+        <button className="button">Sign up</button>
+      </div>
+    </div>
+  );
+}

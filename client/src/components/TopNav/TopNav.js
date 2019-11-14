157c631de1eb60eda2b82a7dc8b5de23efe9b167
Oliver Gomes <ogomesphoto@gmail.com>
2019-11-13 19:01:54 -0500

    style update

--- client/src/components/TopNav/TopNav.js
+++ client/src/components/TopNav/TopNav.js
@@ -1,13 +1,14 @@
 import React from "react";
+import styles from "./TopNav.module.css";
 
 export default function TopNav() {
   return (
-    <div>
-      <div>
+    <div className={styles["top-nav"]}>
+      <div className={styles.left}>
         <span>Write a Review</span>
         <span>Events</span>
       </div>
-      <div>
+      <div className={styles.right}>
         <span>Login</span>
         <button className="button">Sign up</button>
       </div>
